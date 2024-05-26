d602a72e764b48e34247a2eb22317ca46a6d5a56
gouherdanish <gouherdanishiitkgp@gmail.com>
2024-05-26 15:36:45 +0530

    Added Vertical Traversal and Level Order Traversal Codes

--- traversal_vertical_v1.py
+++ traversal_vertical_v1.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 
 class TreeNode:
     def __init__(self,val,left=None,right=None) -> None:
@@ -9,9 +9,9 @@ class TreeNode:
 class Solution:
     def vertical_order(self, root):
         """
-        Approach
+        Approach - Inorder Traversal
         - start from root taking row=0 and column = 0
-        - when you go left child, do row + 1 and column - 1 
+        - when you go left, do row + 1 and column - 1 
         - when you go right, do row + 1 and column + 1
         - Append the node values to corresponding columns in a hashmap 
         - Iterate the hashmap column wise
@@ -25,6 +25,36 @@ class Solution:
                 dfs(node.left, row+1, col-1)
                 dfs(node.right, row+1, col+1)
         dfs(root, 0, 0)
+        mincol, maxcol = min(column_map), max(column_map)
+        ans = []
+        for col in range(mincol,maxcol+1):
+            nodes = column_map[col]
+            nodes.sort()
+            nodes = [node for _, node in nodes]
+            ans.append(nodes)
+        return ans
+
+class Solution1:
+    def vertical_order(self, root):
+        """
+        Approach - Level Order Traversal
+        - start from root taking row=0 and column = 0
+        - when you go left child, do row + 1 and column - 1 
+        - when you go right, do row + 1 and column + 1
+        - Append the node values to corresponding columns in a hashmap 
+        - Iterate the hashmap column wise
+        - sort the nodes at each column row-wise
+        - return
+        """
+        column_map = defaultdict(list)
+        q = deque()
+        row, col = 0, 0
+        q.append((root, row, col))
+        while q:
+            node,row,col = q.popleft()
+            column_map[col].append((row, node.val))
+            if node.left: q.append((node.left, row+1, col-1))
+            if node.right: q.append((node.right, row+1, col+1))
         mincol, maxcol = min(column_map), max(column_map)
         ans = []
         for col in range(mincol,maxcol+1):
@@ -48,4 +78,4 @@ if __name__=='__main__':
     3  6 4
     """
     t = TreeNode(1,TreeNode(2,TreeNode(3),TreeNode(4)),TreeNode(5,TreeNode(6)))
-    print(Solution().vertical_order(t))
+    print(Solution1().vertical_order(t))
