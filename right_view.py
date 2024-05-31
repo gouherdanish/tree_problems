8f11bfdf272fd594c79a458285dc57e7e22f2aca
gouherdanish <gouherdanishiitkgp@gmail.com>
2024-05-31 07:43:45 +0530

    Right View - Trying with Inorder

--- right_view.py
+++ right_view.py
@@ -32,9 +32,36 @@ class Solution:
                 if node.right:
                     q.append(node.right)
         return ans
+    
+class Solution1:
+    def rightView(self, root):
+        """
+        Approach : Inorder
+        - Append root to ans
+        - Go to left only when right is None
 
+        DOES NOT WORK FOR ALL CASES
+            1
+           / \
+          2   3
+         /
+        4
+        
+        For this case, 3 will be chosen and 2 will be ignored so, 4 will not be chosen at all
+        """
+        ans = []
+        while root:
+            ans.append(root.val)
+            if root.right:
+                root = root.right
+            else:
+                root = root.left
+        return ans
+    
 if __name__=='__main__':
     # t = TreeNode(1,TreeNode(2,None,TreeNode(5)),TreeNode(3,None,TreeNode(4)))
     # t = TreeNode(1,None,TreeNode(3))
-    t = None
+    # t = TreeNode(1,TreeNode(2,TreeNode(3)))
+    # t = None
+    t = TreeNode(1,TreeNode(2,TreeNode(4)),TreeNode(3))
     print(Solution().rightView(t))
